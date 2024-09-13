097ec61e97115e274c96da9dfb60ecb7b25cec50
unknown <tamagne13@gmail.com>
2024-09-13 10:02:20 +0300

    Add scripts for simple interest, rectangle area, hours to seconds, and future age calculator

---
+++ python_introduction/hours_to_seconds.py
@@ -0,0 +1,8 @@
+# Define variable
+hours = 2  # Number of hours
+
+# Calculate seconds
+seconds = hours * 3600
+
+# Print the result
+print(f"{hours} hour(s) is {seconds} seconds.")
---
+++ python_introduction/simple_interest.py
@@ -0,0 +1,10 @@
+# Here I can define variables 
+principal = 1000 
+rate =0.05 
+time = 3 
+
+#  calculate simple interest 
+interest = principal*rate*time
+
+# Here I need to print the calculated interest
+print(f"The simple interest is: {interest}")
